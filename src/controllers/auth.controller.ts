3e235135b98950b8abfdbc6e092a7796924ebb5b
Shlomi Elbaz <shlomi.elbaz@outlook.com>
2024-09-16 17:00:51 +0300

    add changes

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -53,6 +53,4 @@ export default class AuthController {
       });
     }
   }
-
-
 }
--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 
 export function welcome(req: Request, res: Response): Response {
-  console.log("Welcome to PayPlus assignment application.")
   return res.json({ message: "Welcome to PayPlus assignment application." });
 }
